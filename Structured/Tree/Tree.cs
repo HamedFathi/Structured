85190292e353c6ea3d2ef218c56c2ba985976e85
hamedfathi <hamedfathi@outlook.com>
2021-11-10 18:49:28 +0330

    feat: Tree data structure added.

---
+++ Structured/Tree/Tree.cs
@@ -0,0 +1,11 @@
+﻿namespace Structured
+{
+    public class Tree<T> where T : class 
+    {
+        public Leaf<T> Root { get; }
+        public Tree(T root)
+        {
+            Root = new Leaf<T>(root);
+        }
+    }
+}
---
+++ Structured/Tree/TreeExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Structured
+{
+    public static class TreeExtensions
+    {
+        public static void TraverseTree<T>(this Leaf<T> leaf, Action<Leaf<T>> action) where T : class
+        {
+            leaf.Children.ToList().ForEach(x =>
+            {
+                action(x);
+                TraverseTree(x, action);
+            });
+        }
+
+        public static IEnumerable<Leaf<T>> FindLeaves<T>(this Leaf<T> leaf, Func<Leaf<T>, bool> whereExpression) where T : class
+        {
+            var flattenedLeaves = new List<Leaf<T>>();
+            leaf.TraverseTree(x =>
+            {
+                flattenedLeaves.Add(x);
+            });
+
+            return flattenedLeaves.Where(whereExpression);
+        }
+
+        public static IEnumerable<Leaf<T>> FlattenTree<T>(this Tree<T> tree) where T : class
+        {
+            var leaves = new List<Leaf<T>> { tree.Root };
+            tree.Root.TraverseTree(x =>
+            {
+                leaves.Add(x);
+            });
+
+            return leaves;
+        }
+
+        public static Leaf<T> FindRoot<T>(this Leaf<T> leaf) where T : class
+        {
+            if (leaf.Parent == null)
+            {
+                return leaf;
+            }
+
+            return FindRoot(leaf.Parent);
+        }
+    }
+}
